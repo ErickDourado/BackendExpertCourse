cac3f5c4596eff490008e971dc898fff6c9e29e2
Erick Dourado <erickdourado_@hotmail.com>
2023-10-03 13:28:27 -0300

    feat: add user repository

---
+++ user-service-api/src/main/java/com/erick/userserviceapi/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.erick.userserviceapi.repository;
+
+import com.erick.userserviceapi.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {}
