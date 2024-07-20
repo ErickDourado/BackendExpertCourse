e0f2b041cb16283251ccf1e4bda1b43ef12a9763
Erick Dourado <erickdourado_@hotmail.com>
2024-07-20 00:12:08 -0300

    feat: add validation to email already exists

--- user-service-api/src/main/java/com/erick/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/com/erick/userserviceapi/service/UserService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import models.exceptions.ResourceNotFoundException;
 import models.requests.CreateUserRequest;
 import models.responses.UserResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,8 +27,17 @@ public class UserService {
     }
 
     public void save(CreateUserRequest request) {
+        verifyIfEmailAlreadyExists(request.email(), null);
         User user = userMapper.toUser(request);
         userRepository.save(user);
+    }
+
+    private void verifyIfEmailAlreadyExists(final String email, final String id) {
+        userRepository.findByEmail(email)
+                .filter(user -> !user.getId().equals(id))
+                .ifPresent(user -> {
+                    throw new DataIntegrityViolationException("Email [ " + email + " ] already exists");
+                });
     }
 
 }
