a247b17af38891db1715dfd9347fc75e024b60ce
Erick Dourado <erickdourado_@hotmail.com>
2024-08-20 22:31:11 -0300

    test: add test to update user, user service

--- user-service-api/src/test/java/com/erick/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/com/erick/userserviceapi/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import com.erick.userserviceapi.mapper.UserMapper;
 import com.erick.userserviceapi.repository.UserRepository;
 import models.exceptions.ResourceNotFoundException;
 import models.requests.CreateUserRequest;
+import models.requests.UpdateUserRequest;
 import models.responses.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.erick.userserviceapi.creator.CreatorUtils.generateMock;
+import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -87,7 +89,7 @@ class UserServiceTest {
 
     @Test
     void whenCallFindAllReturnEmptyListAndThenThrowResourceNotFoundException() {
-        when(repository.findAll()).thenReturn(List.of());
+        when(repository.findAll()).thenReturn(emptyList());
 
         try {
             service.findAll();
@@ -131,9 +133,83 @@ class UserServiceTest {
         }
 
         verify(repository).findByEmail(request.email());
-        verify(mapper, times(0)).toUser(request);
-        verify(encoder, times(0)).encode(request.password());
+        verify(mapper, times(0)).toUser(any(CreateUserRequest.class));
+        verify(encoder, times(0)).encode(anyString());
         verify(repository, times(0)).save(any(User.class));
+    }
+
+    @Test
+    void WhenCallUpdateThenSuccess() {
+        final String id = "1";
+        final User oldUser = generateMock(User.class).withId(id);
+        final User newUser = generateMock(User.class).withId(id);
+        final UserResponse response = generateMock(UserResponse.class);
+        final UpdateUserRequest request = generateMock(UpdateUserRequest.class);
+
+        when(repository.findById(id)).thenReturn(Optional.of(oldUser));
+        when(repository.findByEmail(request.email())).thenReturn(Optional.of(oldUser));
+        when(mapper.updateToUser(request, oldUser)).thenReturn(newUser);
+        when(encoder.encode(request.password())).thenReturn("encoded");
+        when(repository.save(any(User.class))).thenReturn(newUser);
+        when(mapper.toUserResponse(newUser)).thenReturn(response);
+
+        final UserResponse userResponse = service.update(request, id);
+
+        assertNotNull(userResponse);
+        assertEquals(UserResponse.class, userResponse.getClass());
+
+        verify(repository).findById(id);
+        verify(repository).findByEmail(request.email());
+        verify(mapper).updateToUser(request, oldUser);
+        verify(encoder).encode(request.password());
+        verify(repository).save(any(User.class));
+        verify(mapper).toUserResponse(newUser);
+    }
+
+    @Test
+    void whenCallUpdateWithInvalidIdThenThrowResourceNotFoundException() {
+        final String id = "1";
+        final UpdateUserRequest request = generateMock(UpdateUserRequest.class);
+
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        try {
+            service.update(request, id);
+        } catch (Exception e) {
+            assertEquals(ResourceNotFoundException.class, e.getClass());
+            assertEquals("Object not found. Id: 1, Type: UserResponse", e.getMessage());
+        }
+
+        verify(repository).findById(id);
+        verify(repository, times(0)).findByEmail(anyString());
+        verify(mapper, times(0)).updateToUser(any(UpdateUserRequest.class), any(User.class));
+        verify(encoder, times(0)).encode(anyString());
+        verify(repository, times(0)).save(any(User.class));
+        verify(mapper, times(0)).toUserResponse(any(User.class));
+    }
+
+    @Test
+    void whenCallUpdateWithInvalidEmailThenThrowDataIntegrityViolationException() {
+        final String id = "1";
+        final User oldUser = generateMock(User.class);
+        final UpdateUserRequest request = generateMock(UpdateUserRequest.class);
+
+        when(repository.findById(id)).thenReturn(Optional.of(oldUser));
+        when(repository.findByEmail(request.email())).thenReturn(Optional.of(generateMock(User.class)));
+
+        try {
+            service.update(request, id);
+        } catch (Exception e) {
+            assertEquals(DataIntegrityViolationException.class, e.getClass());
+            assertEquals("Email [ " + request.email() + " ] already exists", e.getMessage()); 
+        }
+
+        verify(repository).findById(id);
+        verify(repository).findByEmail(request.email());
+        verify(mapper, times(0)).updateToUser(any(UpdateUserRequest.class), any(User.class));
+        verify(encoder, times(0)).encode(anyString());
+        verify(repository, times(0)).save(any(User.class));
+        verify(mapper, times(0)).toUserResponse(any(User.class));
     }
 
 }
