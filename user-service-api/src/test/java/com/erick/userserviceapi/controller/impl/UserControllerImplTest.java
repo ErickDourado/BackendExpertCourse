329bc996e6c4f7603341b64217846a5bfd97a027
Erick Dourado <erickdourado_@hotmail.com>
2024-08-20 22:58:05 -0300

    test: add integration test to find user by id with success, user controller

---
+++ user-service-api/src/test/java/com/erick/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -0,0 +1,44 @@
+package com.erick.userserviceapi.controller.impl;
+
+import com.erick.userserviceapi.entity.User;
+import com.erick.userserviceapi.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.erick.userserviceapi.creator.CreatorUtils.generateMock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserControllerImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    void testFindByIdWithSuccess() throws Exception {
+        final User entity = generateMock(User.class);
+        final String userId = repository.save(entity).getId();
+
+        mockMvc.perform(get("/api/users/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId))
+                .andExpect(jsonPath("$.name").value(entity.getName()))
+                .andExpect(jsonPath("$.email").value(entity.getEmail()))
+                .andExpect(jsonPath("$.password").value(entity.getPassword()))
+                .andExpect(jsonPath("$.profiles").isArray());
+
+        repository.deleteById(userId);
+    }
+
+}
