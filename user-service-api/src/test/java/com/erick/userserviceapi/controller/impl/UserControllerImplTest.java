9b3932c886fbae1724f2f3e4ed2a9bf73ef9fdbf
Erick Dourado <erickdourado_@hotmail.com>
2024-08-21 21:35:26 -0300

    test: add integration test to find all users with success, user controller

--- user-service-api/src/test/java/com/erick/userserviceapi/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/com/erick/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static com.erick.userserviceapi.creator.CreatorUtils.generateMock;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -56,6 +58,22 @@ class UserControllerImplTest {
                 .andExpect(jsonPath("$.error").value(NOT_FOUND.getReasonPhrase()))
                 .andExpect(jsonPath("$.message").value(expectedMessage))
                 .andExpect(jsonPath("$.path").value(path));
+    }
+
+    @Test
+    void testFindAllWithSuccess() throws Exception {
+        final List<User> entities = List.of(generateMock(User.class), generateMock(User.class));
+        repository.saveAll(entities);
+
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0]").isNotEmpty())
+                .andExpect(jsonPath("$[0].profiles").isArray())
+                .andExpect(jsonPath("$[1]").isNotEmpty())
+                .andExpect(jsonPath("$[1].profiles").isArray());
+
+        repository.deleteAll(entities);
     }
 
 }
