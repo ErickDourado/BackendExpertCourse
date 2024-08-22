35d9d865ae7f0b7c3e0461ea3983407ad14d2f9e
Erick Dourado <erickdourado_@hotmail.com>
2024-08-21 21:22:43 -0300

    test: add integration test to find user by id with not found exception, user controller

--- user-service-api/src/test/java/com/erick/userserviceapi/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/com/erick/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -2,6 +2,7 @@ package com.erick.userserviceapi.controller.impl;
 
 import com.erick.userserviceapi.entity.User;
 import com.erick.userserviceapi.repository.UserRepository;
+import models.responses.UserResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.erick.userserviceapi.creator.CreatorUtils.generateMock;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,6 +41,21 @@ class UserControllerImplTest {
                 .andExpect(jsonPath("$.profiles").isArray());
 
         repository.deleteById(userId);
+    }
+
+    @Test
+    void testFindByIdWithNotFoundException() throws Exception {
+        final String id = "123";
+        final String expectedMessage = "Object not found. Id: " + id + ", Type: " + UserResponse.class.getSimpleName();
+        final String path = "/api/users/" + id;
+
+        mockMvc.perform(get(path))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.status").value(NOT_FOUND.value()))
+                .andExpect(jsonPath("$.error").value(NOT_FOUND.getReasonPhrase()))
+                .andExpect(jsonPath("$.message").value(expectedMessage))
+                .andExpect(jsonPath("$.path").value(path));
     }
 
 }
