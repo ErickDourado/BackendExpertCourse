64b60be949422dbab11608ec762773d8f2c0385a
Erick Dourado <erickdourado_@hotmail.com>
2025-08-30 03:33:13 -0300

    feat: add authentication impl

---
+++ auth-service-api/src/main/java/com/erick/authserviceapi/model/User.java
@@ -0,0 +1,19 @@
+package com.erick.authserviceapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import models.enums.ProfileEnum;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profiles;
+
+}
---
+++ auth-service-api/src/main/java/com/erick/authserviceapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.erick.authserviceapi.repository;
+
+import com.erick.authserviceapi.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(final String email);
+
+}
