c0bd28e8d7414bf665bec1e066683aeb483774e1
Erick Dourado <erickdourado_@hotmail.com>
2025-09-13 01:11:37 -0300

    refactor: update java/spring version and adjusts deprecated methods in security config

--- auth-service-api/src/main/java/com/erick/authserviceapi/config/SecurityConfig.java
+++ auth-service-api/src/main/java/com/erick/authserviceapi/config/SecurityConfig.java
@@ -3,29 +3,23 @@ package com.erick.authserviceapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 public class SecurityConfig {
 
+    private static final String[] AUTH_WHITELIST = {"/v3/api-docs/**", "/swagger-ui/**", "/api/auth/**", "/actuator/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf().disable()
-                .authorizeHttpRequests(http -> http
-                        .requestMatchers(
-                                new AntPathRequestMatcher("/swagger-ui/**"),
-                                new AntPathRequestMatcher("/v3/api-docs/**"),
-                                new AntPathRequestMatcher("/api/auth/**")
-                        )
-                        .permitAll().anyRequest().authenticated()
-                )
-                .sessionManagement().sessionCreationPolicy(STATELESS)
-                .and().build();
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(http -> http.requestMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
     @Bean
